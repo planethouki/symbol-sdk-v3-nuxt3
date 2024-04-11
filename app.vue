6e4f571bc9ba003ea9df66ff9cf12a1067bc0cba
AnthonyLaw <yc-law1015@hotmail.com>
2024-04-11 16:13:51 +0800

    fix cannot find module error

--- app.vue
+++ app.vue
@@ -10,7 +10,7 @@
 
 <script setup lang="ts">
 import { PrivateKey } from 'symbol-sdk';
-import { SymbolFacade, Network, KeyPair } from 'symbol-sdk/symbol';
+import { SymbolFacade, Network, KeyPair } from 'symbol-sdk/ts/src/symbol/index';
 
 const config = useRuntimeConfig()
 
